864cfaf09c05478219d4dafd0b9490adc51844ba
Cole Holyoake <coleholyoake@live.com>
2016-05-01 21:22:56 +1200

    Exporting & Importing A Child Component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ContactsList from './ContactsList';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Contact List</h1>
+				<ContactsList />
+			</div>
+		)
+	}
+}
+
+React.render(<App />, document.getElementById('app'));
+
---
+++ src/ContactsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ContactsList extends React.Component {
+	render() {
+		return (
+			<ul>
+				<li>Cole 555 555 5555</li>
+			</ul>
+		)
+	}
+}
+
+export default ContactsList;
+
+
